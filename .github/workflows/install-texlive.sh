7bf35e61099551b5400bacb505980ad3267a2bee
Zeping Lee <zepinglee@gmail.com>
2020-02-28 10:10:15 +0800

    Update CI

---
+++ .github/workflows/install-texlive.sh
@@ -0,0 +1,20 @@
+REMOTE="https://mirrors.rit.edu/CTAN/systems/texlive/tlnet";
+INSTALL="/tmp/install-texlive";
+
+mkdir -p "$INSTALL";
+curl -sSL "$REMOTE/install-tl-unx.tar.gz" | tar -xz -C "$INSTALL" \
+    --strip-components=1;
+"$INSTALL/install-tl" -profile .github/workflows/texlive.profile;
+
+export PATH="/tmp/texlive/bin/x86_64-linux:$PATH";
+
+XETEX_PKGS="fontname fontspec l3packages xetex";
+CTEX_PKGS="cjk ctex environ ms trimspaces ulem xecjk zhnumber";
+HYPERREF_PKGS="bitset letltxmacro pdfescape pdflscape";
+
+BIN_PKGS="latexmk l3build";
+REQUIRED_PKGS="$XETEX_PKGS $CTEX_PKGS";
+FONT_PKGS="fandol";
+DOC_PKGS="booktabs caption $HYPERREF_PKGS listings xcolor";
+
+tlmgr install $BIN_PKGS $REQUIRED_PKGS $FONT_PKGS $DOC_PKGS;
