61c74429d330ee15353ceace5a66ce3d3d634101
Zeping Lee <zepinglee@gmail.com>
2026-02-22 13:40:24 +0800

    Update tests

--- tests/config-chapterbib.lua
+++ tests/config-chapterbib.lua
@@ -1,8 +1,13 @@
-testfiledir = "./test/testfiles-chapterbib"
+---@diagnostic disable: lowercase-global
+
+-- Configuration file for use with "l3build"
+
+testfiledir = "./tests/testfiles-chapterbib"
 testsuppdir = testfiledir .. "/support"
 
+test_order = {"log"}
 checkruns = 3
 
 function runtest_tasks(name)
-    return "bibtex -terse section1; bibtex -terse section2"
+  return "bibtex -terse chapter1; bibtex -terse chapter2"
 end
